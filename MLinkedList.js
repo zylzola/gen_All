5acae5f5f270c6563e9ffbde3dce4a3835ba2b1e
zylzola <45475703+zylzola@users.noreply.github.com>
2023-11-21 18:53:59 +0800

    Update MLinkedList.js

--- MLinkedList.js
+++ MLinkedList.js
@@ -33,3 +33,40 @@ let head = {
 };
 reverseList(head);
 console.log(head);
+
+/***
+ * 给定一个链表，反转链表中相邻的两个节点
+ * 1->2->3->4->5->null
+ * */
+function reverseList2(head) {
+  let prev = null;
+  let curr = head;
+  while (curr && curr.next) {
+    let next = curr.next.next;
+    curr.next.next = curr;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  head = prev;
+  return head;
+}
+//验证reverseList2
+let head2 = {
+  val: 1,
+  next: {
+    val: 2,
+    next: {
+      val: 3,
+      next: {
+        val: 4,
+        next: {
+          val: 5,
+          next: null,
+        },
+      },
+    },
+  },
+};
+reverseList2(head2);
+console.log(head2);
